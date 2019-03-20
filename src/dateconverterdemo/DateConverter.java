612e1b7eccbfc7ae70edaf22315555f08537a9e1
Robert Kiss-Vincze <rkissvincze@precognox.com>
2019-03-20 11:25:14 +0100

    Remote changed

--- src/dateconverterdemo/DateConverter.java
+++ src/dateconverterdemo/DateConverter.java
@@ -5,14 +5,14 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
+/** Date normalizer
  * @author rkissvincze
  */
 class DateConverter {
 
-    private static final String DEFAULT_OUTPUT_DATE_FORMAT = "yyyy-MM-dd.";
+    private static final String DEFAULT_OUTPUT_DATE_FORMAT = "yyyy-MM-dd";
     private static final String[] DATE_FORMATS_HUN = {"yyyy.MM.dd", "yyyy. MM. dd", "yyyy MM dd", "yyyy-MM-dd",
-            "yyyy/MM/dd", "yyyy MM dd", "yyyy MM. dd"};
+        "yyyy/MM/dd", "yyyy MM dd", "yyyy MM. dd", "yyyy. MM dd", "yyyy. MM dd.", "yyyy MM dd."};
     private static final String[] DATE_FORMATS_ENG = {"MM/dd/yyyy", "dd.MM.yyyy", "MM. dd yyyy", "dd MM. yyyy"};
     private static final String DATE_MATCH_REGEX = "[a-zA-Z]";
     private static final String HUN_FORMAT_REGEX = "^\\d{4}";
@@ -21,7 +21,7 @@ class DateConverter {
     static {
         DATE_MONTH_REGEX.put("jan[.]?[á-úa-z]*", "01");
         DATE_MONTH_REGEX.put("feb[.]?[á-úa-z]*", "02");
-        DATE_MONTH_REGEX.put("mar[.]?\\w*|már[.]?\\w*", "03");
+        DATE_MONTH_REGEX.put("mar[.]?\\w*|már[.]?\\w*|marc[.]?\\w*|márc[.]?\\w*", "03");
         DATE_MONTH_REGEX.put("apr[.]?\\w*|ápr[.]?\\w*", "04");
         DATE_MONTH_REGEX.put("maj[.]?\\w*|máj[.]?\\w*|may[.]?\\w*", "05");
         DATE_MONTH_REGEX.put("jun[.]?\\w*|jún[.]?\\w*", "06");
@@ -32,44 +32,55 @@ class DateConverter {
         DATE_MONTH_REGEX.put("nov[.]?\\w*", "11");
         DATE_MONTH_REGEX.put("dec[.]?\\w*", "12");
     }
+/*** Normalize the incoming dates
+ * 
+ * @param dateInString Date in String format
+ * @param dateOutFormat Date output format, defalult: yyyy-MM-dd
+ * @return Normalized Date in String, null if date is not suitable
+ * @throws -
+ */
 
-
-    static String normalizeDate(String dateInString) throws Exception {
+    static String normalizeDate(String dateInString, String dateOutFormat) throws Exception {
         if (dateInString == null || dateInString.trim().isEmpty()) {
             return null;
         }
-        String dateText = dateInString.trim();
+        if(dateOutFormat == null || dateOutFormat.trim().isEmpty()){
+            dateOutFormat = DEFAULT_OUTPUT_DATE_FORMAT;
+        }
+        String dateText = dateInString.trim().toLowerCase();
         Pattern pattern = Pattern.compile(HUN_FORMAT_REGEX);
         Matcher matcher = pattern.matcher(dateText);
         String[] dateFormants = matcher.find() ? DATE_FORMATS_HUN : DATE_FORMATS_ENG;
-        System.out.println("used date for.. " + dateText +" is " + Arrays.toString(dateFormants));
-        System.out.println("A MAP:" + DATE_MONTH_REGEX.size());
+//        System.out.println("used date for.. " + dateText +" is " + Arrays.toString(dateFormants));
+//        System.out.println("A MAP:" + DATE_MONTH_REGEX.size());
         for (String dateFormatIn : dateFormants) {
             try {
-                return transformDate(dateText, dateFormatIn, DEFAULT_OUTPUT_DATE_FORMAT);
+                return transformDate(dateText, dateFormatIn, dateOutFormat);
             } catch (Exception ignored) {
             }
         }
         pattern = Pattern.compile(DATE_MATCH_REGEX);
         matcher = pattern.matcher(dateText);
         if (matcher.find()) {
-            System.out.println("FINDE by..." + dateText);
+//            System.out.println("FINDE by..." + dateText);
             try {
-                return transformDateWithText(dateText, DEFAULT_OUTPUT_DATE_FORMAT);
+                return transformDateWithText(dateText, dateOutFormat);
             } catch (Exception ignored) {
             }
         }
-        throw new Exception(String.format("Invalid date format! Valid formats: %s; Date text: %s;", Arrays.toString(dateFormants), dateInString));
+//        throw new Exception(String.format("Invalid date format! Valid formats: %s; Date text: %s;", Arrays.toString(dateFormants), dateInString));
+// Because of I don't want Exception...simple drop the unsuitable date
+        return null;
     }
 
-    private static String transformDateWithText(String dateText, String defaultOutputDateFormat) throws Exception {
+    private static String transformDateWithText(String dateText, String dateOutFormat) throws Exception {
         Pattern pattern;
         Matcher matcher;
         for (Map.Entry<String, String> entry : DATE_MONTH_REGEX.entrySet()) {
             pattern = Pattern.compile(entry.getKey());
             matcher = pattern.matcher(dateText);
             if (matcher.find()) {
-                return normalizeDate(dateText.replaceAll(entry.getKey(), entry.getValue() + "."));
+                return normalizeDate(dateText.replaceAll(entry.getKey(), entry.getValue() ), dateOutFormat);
             }
         }
         return null;
