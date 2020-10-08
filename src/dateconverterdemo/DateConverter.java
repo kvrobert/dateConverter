30c24207de39b2721dce7dd5dc700a0f10f5b1a4
Robert Kiss-Vincze <rkissvincze@precognox.com>
2020-10-08 16:30:23 +0200

    Refactoring, handling new date formats, date replacements

--- src/dateconverterdemo/DateConverter.java
+++ src/dateconverterdemo/DateConverter.java
@@ -6,22 +6,26 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
+ * Date normalizer
+ *
  * @author rkissvincze
  */
 class DateConverter {
 
-    private static final String DEFAULT_OUTPUT_DATE_FORMAT = "yyyy-MM-dd.";
+    private static final String DEFAULT_OUTPUT_DATE_FORMAT = "yyyy-MM-dd";
     private static final String[] DATE_FORMATS_HUN = {"yyyy.MM.dd", "yyyy. MM. dd", "yyyy MM dd", "yyyy-MM-dd",
-            "yyyy/MM/dd", "yyyy MM dd", "yyyy MM. dd"};
-    private static final String[] DATE_FORMATS_ENG = {"MM/dd/yyyy", "dd.MM.yyyy", "MM. dd yyyy", "dd MM. yyyy"};
+            "yyyy/MM/dd", "yyyy MM dd", "yyyy MM. dd", "yyyy. MM dd", "yyyy. MM dd.", "yyyy MM dd.", "yyyy MM", "yyyy"};
+    private static final String[] DATE_FORMATS_ENG = {"MM/dd/yy", "MM/dd/yyyy", "MM. dd yyyy", "MM yyyy", "MM/yyyy", "MM-yyyy"};
+    private static final String[] DATE_FORMATS_GER = {"dd/MM/yy", "dd.MM.yyyy", "dd MM. yyyy", "dd/MM/yyyy", "dd-MM-yyyy"};
     private static final String DATE_MATCH_REGEX = "[a-zA-Z]";
     private static final String HUN_FORMAT_REGEX = "^\\d{4}";
+    private static final String FOREIGN_FORMAT_REGEX = "^\\d{2}";
     private static final Map<String, String> DATE_MONTH_REGEX = new HashMap<>();
 
     static {
         DATE_MONTH_REGEX.put("jan[.]?[á-úa-z]*", "01");
         DATE_MONTH_REGEX.put("feb[.]?[á-úa-z]*", "02");
-        DATE_MONTH_REGEX.put("mar[.]?\\w*|már[.]?\\w*", "03");
+        DATE_MONTH_REGEX.put("mar[.]?\\w*|már[.]?\\w*|marc[.]?\\w*|márc[.]?\\w*", "03");
         DATE_MONTH_REGEX.put("apr[.]?\\w*|ápr[.]?\\w*", "04");
         DATE_MONTH_REGEX.put("maj[.]?\\w*|máj[.]?\\w*|may[.]?\\w*", "05");
         DATE_MONTH_REGEX.put("jun[.]?\\w*|jún[.]?\\w*", "06");
@@ -33,46 +37,70 @@ class DateConverter {
         DATE_MONTH_REGEX.put("dec[.]?\\w*", "12");
     }
 
+    /*** Normalize the incoming dates
+     *
+     * @param dateInString Date in String format
+     * @param dateOutFormat Date output format, defalult: yyyy-MM-dd
+     * @return Normalized Date in String, null if date is not suitable
+     * @throws -
+     */
 
-    static String normalizeDate(String dateInString) throws Exception {
+    static String normalizeDate(String dateInString, String dateOutFormat) {
         if (dateInString == null || dateInString.trim().isEmpty()) {
             return null;
         }
-        String dateText = dateInString.trim();
+        if (dateOutFormat == null || dateOutFormat.trim().isEmpty()) {
+            dateOutFormat = DEFAULT_OUTPUT_DATE_FORMAT;
+        }
+        String dateText = dateInString.trim().toLowerCase();
         Pattern pattern = Pattern.compile(HUN_FORMAT_REGEX);
         Matcher matcher = pattern.matcher(dateText);
-        String[] dateFormants = matcher.find() ? DATE_FORMATS_HUN : DATE_FORMATS_ENG;
-        System.out.println("used date for.. " + dateText +" is " + Arrays.toString(dateFormants));
-        System.out.println("A MAP:" + DATE_MONTH_REGEX.size());
+        String[] dateFormants; // = matcher.find() ? DATE_FORMATS_HUN : DATE_FORMATS_ENG;
+
+        dateText = normalizeDateText(dateText, dateOutFormat);
+
+        if (matcher.find()) {
+            dateFormants = DATE_FORMATS_HUN;
+        } else {
+            if (isGermanDateFormat(dateText)) {
+                dateFormants = DATE_FORMATS_GER;
+            } else {
+                dateFormants = DATE_FORMATS_ENG;
+            }
+        }
         for (String dateFormatIn : dateFormants) {
             try {
                 return transformDate(dateText, dateFormatIn, DEFAULT_OUTPUT_DATE_FORMAT);
             } catch (Exception ignored) {
             }
         }
-        pattern = Pattern.compile(DATE_MATCH_REGEX);
-        matcher = pattern.matcher(dateText);
-        if (matcher.find()) {
-            System.out.println("FINDE by..." + dateText);
-            try {
-                return transformDateWithText(dateText, DEFAULT_OUTPUT_DATE_FORMAT);
-            } catch (Exception ignored) {
-            }
-        }
-        throw new Exception(String.format("Invalid date format! Valid formats: %s; Date text: %s;", Arrays.toString(dateFormants), dateInString));
+        return null;
     }
 
-    private static String transformDateWithText(String dateText, String defaultOutputDateFormat) throws Exception {
+    private static boolean isGermanDateFormat(String dateText) {
+        Pattern germanPattern = Pattern.compile(FOREIGN_FORMAT_REGEX);
+        Matcher germanMatcher = germanPattern.matcher(dateText);
+        if (germanMatcher.find()) {
+            int germanDays = Integer.parseInt(dateText.trim().substring(0, 2));
+            return germanDays > 12;
+        } else {
+            return false;
+        }
+    }
+
+    private static String normalizeDateText(String dateText, String dateOutFormat) {
+
         Pattern pattern;
         Matcher matcher;
         for (Map.Entry<String, String> entry : DATE_MONTH_REGEX.entrySet()) {
             pattern = Pattern.compile(entry.getKey());
             matcher = pattern.matcher(dateText);
+
             if (matcher.find()) {
-                return normalizeDate(dateText.replaceAll(entry.getKey(), entry.getValue() + "."));
+                return dateText.replaceAll(entry.getKey(), entry.getValue());
             }
         }
-        return null;
+        return dateText;
     }
 
     private static final String DEFAULT_TIMEZONE_ID = "UTC+02:00";
@@ -86,7 +114,7 @@ class DateConverter {
         return dateAsString(date, dateFormatOut);
     }
 
-    private static String dateAsString(Date date, String dateFormatOut) throws Exception {
+    private static String dateAsString(Date date, String dateFormatOut) {
         SimpleDateFormat formatter = new SimpleDateFormat(dateFormatOut, DEFAULT_LOCALE);
         formatter.setTimeZone(DEFAULT_TIMEZONE);
         return formatter.format(date);
